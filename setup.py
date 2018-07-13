042feeeaa3fb8a00b870af4c9493cecb21882a0e
Dan Foreman-Mackey <danfm@nyu.edu>
2018-07-13 16:06:44 -0400

    cuda interp

--- setup.py
+++ setup.py
@@ -34,7 +34,7 @@ def construct_op(name):
             include_dirs=[path, "include"],
             extra_compile_args={
                 "cxx": flags,
-                "nvcc": flags + ["--expt-relaxed-constexpr", "-shared", "-x", "cu", "-Xcompiler"],
+                "nvcc": flags + ["--expt-relaxed-constexpr"],
             },
             extra_link_args=flags,
         )
