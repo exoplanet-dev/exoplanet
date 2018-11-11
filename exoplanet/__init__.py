686ded3edd128de46c7029ae1673e590b334a7f6
Dan F-M <foreman.mackey@gmail.com>
2018-11-11 07:28:40 -0500

    cleaning up

--- exoplanet/__init__.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from __future__ import division, print_function
-
-__all__ = ["transit", "kepler", "interp"]
-
-from . import transit
-from . import kepler
-from . import interp
