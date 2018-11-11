79e083d921364a695c1e5dbf37f17f770f13225c
Dan F-M <foreman.mackey@gmail.com>
2018-11-11 08:00:07 -0500

    versioning

---
+++ exoplanet/__init__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+__version__ = "0.0.1"
+
+try:
+    __EXOPLANET_SETUP__
+except NameError:
+    __EXOPLANET_SETUP__ = False
+
+if not __EXOPLANET_SETUP__:
+    __all__ = []
