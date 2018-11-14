e8be27be151ef559524d85800d4c83777a4fe067
Dan F-M <foreman.mackey@gmail.com>
2018-11-13 22:54:25 -0500

    refactoring kepler op

--- exoplanet/theano_ops/kepler/orbit.py
+++ exoplanet/theano_ops/kepler/orbit.py
@@ -2,143 +2,21 @@
 
 from __future__ import division, print_function
 
-__all__ = ["KeplerOp", "get_eccentric_anomaly", "KeplerianOrbit"]
+__all__ = ["KeplerianOrbit"]
 
 import numpy as np
 
-from theano import gof
 import theano.tensor as tt
 
+from astropy import constants
+from astropy import units as u
 
-class KeplerOp(gof.Op):
+from .solver import KeplerOp
 
-    __props__ = ("tol", "maxiter")
-
-    def __init__(self, tol=1e-8, maxiter=2000, **kwargs):
-        self.tol = tol
-        self.maxiter = maxiter
-        super(KeplerOp, self).__init__(**kwargs)
-
-    def make_node(self, mean_anom, eccen):
-        in_args = [tt.as_tensor_variable(mean_anom),
-                   tt.as_tensor_variable(eccen)]
-        return gof.Apply(self, in_args, [in_args[0].type()])
-
-    def infer_shape(self, node, shapes):
-        return shapes[0],
-
-    def c_code_cache_version(self):
-        return (0, 0, 1)
-
-    def grad(self, inputs, gradients):
-        M, e = inputs
-        E = self(M, e)
-        bM = gradients[0] / (1.0 - e * tt.cos(E))
-        be = tt.sin(E) * bM
-        return [bM, be]
-
-    def c_support_code_apply(self, node, name):
-        dtype_mean_anom = node.inputs[0].dtype
-        dtype_eccen = node.inputs[1].dtype
-        dtype_eccen_anom = node.outputs[0].dtype
-
-        c_support_code = """
-        inline npy_%(dtype_eccen_anom)s solve_kepler_%(name)s (
-            npy_%(dtype_mean_anom)s M, npy_%(dtype_eccen)s e,
-            int maxiter, float tol
-        ) {
-            typedef npy_%(dtype_eccen_anom)s T;
-            T E0 = M, E = M;
-            if (fabs(e) < tol) return E;
-            for (int i = 0; i < maxiter; ++i) {
-                T g = E0 - e * sin(E0) - M, gp = 1.0 - e * cos(E0);
-                T delta = g / (gp + tol);
-                delta = (fabs(delta) < T(1)) ? delta : delta / fabs(delta);
-                E = E0 - delta;
-                if (fabs(E - E0) <= T(tol)) {
-                    return E;
-                }
-                E0 = E;
-            }
-            return E;
-        }
-        """
-        return c_support_code % locals()
-
-    def c_code(self, node, name, inp, out, sub):
-        tol = self.tol
-        maxiter = self.maxiter
-
-        mean_anom, eccen = inp
-        eccen_anom, = out
-
-        dtype_mean_anom = node.inputs[0].dtype
-        dtype_eccen = node.inputs[1].dtype
-        dtype_eccen_anom = node.outputs[0].dtype
-
-        itemsize_mean_anom = np.dtype(dtype_mean_anom).itemsize
-        itemsize_eccen = np.dtype(dtype_eccen).itemsize
-        itemsize_eccen_anom = np.dtype(dtype_eccen_anom).itemsize
-
-        typenum_eccen_anom = np.dtype(dtype_eccen_anom).num
-
-        fail = sub['fail']
-
-        c_code = """
-        npy_intp size = PyArray_SIZE(%(mean_anom)s);
-
-        npy_%(dtype_mean_anom)s* mean_anom;
-        npy_%(dtype_eccen)s* eccen;
-        npy_%(dtype_eccen_anom)s* eccen_anom;
-
-        // Validate that the inputs have the same shape
-        if ( !PyArray_SAMESHAPE(%(mean_anom)s, %(eccen)s) )
-        {
-            PyErr_Format(PyExc_ValueError, "shape mismatch");
-            %(fail)s;
-        }
-
-        // Validate that the output storage exists and has the same
-        // shape.
-        if (NULL == %(eccen_anom)s ||
-            !PyArray_SAMESHAPE(%(mean_anom)s, %(eccen_anom)s))
-        {
-            Py_XDECREF(%(eccen_anom)s);
-            %(eccen_anom)s = (PyArrayObject*)PyArray_EMPTY(
-                PyArray_NDIM(%(mean_anom)s),
-                PyArray_DIMS(%(mean_anom)s),
-                %(typenum_eccen_anom)s,
-                0);
-
-            if (!%(eccen_anom)s) {
-                %(fail)s;
-            }
-        }
-
-        mean_anom = (npy_%(dtype_mean_anom)s*)PyArray_DATA(%(mean_anom)s);
-        eccen = (npy_%(dtype_eccen)s*)PyArray_DATA(%(eccen)s);
-        eccen_anom = (npy_%(dtype_eccen_anom)s*)PyArray_DATA(%(eccen_anom)s);
-
-        for (npy_intp i = 0; i < size; ++i) {
-            eccen_anom[i] = solve_kepler_%(name)s (mean_anom[i], eccen[i],
-                %(maxiter)s, %(tol)s);
-        }
-        """
-
-        return c_code % locals()
-
-
-get_eccentric_anomaly = KeplerOp()
-
-
-# from astropy import constants
-# from astropy import units as u
-
-# (constants.M_sun / constants.R_sun**3).to(u.g / u.cm**3).value
-gcc_to_sun = 5.905466576479012  # M_sun / R_sun^3 in g / cm^3
-
-# constants.G.to(u.R_sun**3 / u.M_sun / u.day**2).value
-G_grav = 2942.206217504419  # R_sun^3 / M_sun / day^2
+gcc_to_sun = (constants.M_sun / constants.R_sun**3).to(u.g / u.cm**3).value
+G_grav = constants.G.to(u.R_sun**3 / u.M_sun / u.day**2).value
+sqrt_G_grav_ms = np.sqrt(constants.G.to(
+    u.R_sun / u.M_sun * u.m**2 / u.second**2).value)
 
 
 class KeplerianOrbit(object):
@@ -165,7 +43,7 @@ class KeplerianOrbit(object):
         self.a_star = self.a * self.m_planet / self.m_total
         self.a_planet = -self.a * self.m_star / self.m_total
 
-        self.K0 = tt.sqrt(G_grav/(self.m_total*self.a))
+        self.K0 = sqrt_G_grav_ms / tt.sqrt(self.m_total*self.a)
         self.cos_incl = tt.cos(incl)
         self.sin_incl = tt.sin(incl)
 
@@ -276,22 +154,27 @@ class KeplerianOrbit(object):
     def _get_true_anomaly(self, t):
         M = (tt.shape_padright(t) - self.tref) * self.n
         if self.ecc is None:
-            return M
+            return tt.squeeze(M)
         E = self.kepler_op(M, self.ecc + tt.zeros_like(M))
         f = 2.0 * tt.arctan2(tt.sqrt(1.0 + self.ecc) * tt.tan(0.5*E),
                              tt.sqrt(1.0 - self.ecc) + tt.zeros_like(E))
-        return f
+        return tt.squeeze(f)
 
     def _get_position(self, a, t):
         f = self._get_true_anomaly(t)
         cosf = tt.cos(f)
-        r = a * (1.0 - self.ecc**2) / (1 + self.ecc * cosf)
+        if self.ecc is None:
+            r = a
+        else:
+            r = a * (1.0 - self.ecc**2) / (1 + self.ecc * cosf)
         return self._rotate_vector(r * cosf, r * tt.sin(f))
 
     def get_planet_position(self, t):
+        """The planets' positions in Solar radii"""
         return self._get_position(self.a_planet, t)
 
     def get_star_position(self, t):
+        """The star's position in Solar radii"""
         return self._get_position(self.a_star, t)
 
     def _get_velocity(self, m, t):
@@ -300,7 +183,9 @@ class KeplerianOrbit(object):
         return self._rotate_vector(-K*tt.sin(f), K*(tt.cos(f) + self.ecc))
 
     def get_planet_velocity(self, t):
+        """The planets' velocities in m/s"""
         return self._get_velocity(-self.m_star, t)
 
     def get_star_velocity(self, t):
-        return self._get_position(self.m_planet, t)
+        """The star's velocity in m/s"""
+        return self._get_velocity(self.m_planet, t)
