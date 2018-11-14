e74bc62c83d1e797a8d4d3fa20c881748ff65648
Dan F-M <foreman.mackey@gmail.com>
2018-11-14 18:11:04 -0500

    ci conda

--- .ci/travis.sh
+++ .ci/travis.sh
@@ -1,15 +1,16 @@
 # Miniconda (cached)
-export PATH="$HOME/miniconda/bin:$PATH"
 if ! command -v conda > /dev/null; then
-      wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O miniconda.sh;
-      bash miniconda.sh -b -p $HOME/miniconda -u;
-      conda config --add channels conda-forge;
-      conda config --set always_yes yes;
-      conda update --all;
-      conda create --yes -n test python=$PYTHON_VERSION
-      conda activate test
-      conda install -c conda-forge numpy=$NUMPY_VERSION scipy astropy setuptools pytest pytest-cov pip;
-      pip install batman-package
+    wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O miniconda.sh;
+    bash miniconda.sh -b -p $HOME/miniconda -u;
+    . $HOME/miniconda/etc/profile.d/conda.sh
+    conda activate
+    conda config --add channels conda-forge;
+    conda config --set always_yes yes;
+    conda update --all;
+    conda create --yes -n test python=$PYTHON_VERSION
+    conda activate test
+    conda install -c conda-forge numpy=$NUMPY_VERSION scipy astropy setuptools pytest pytest-cov pip pymc3 theano;
+    pip install batman-package
 fi
 
 # Display some info
