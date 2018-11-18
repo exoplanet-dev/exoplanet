92d0190e81c723fbaff10fa4348d825df8cd9a46
Dan F-M <foreman.mackey@gmail.com>
2018-11-17 23:00:58 -0500

    refactoring kepler

--- exoplanet/sampling.py
+++ exoplanet/sampling.py
@@ -15,35 +15,36 @@ from pymc3.step_methods.hmc import quadpotential as quad
 class TuningSchedule(object):
     # Ref: src/stan/mcmc/windowed_adaptation.hpp in stan repo
 
-    def __init__(self, warmup=1000, start=75, finish=50, window=25,
-                 dense=True):
+    def __init__(self, start=75, finish=50, window=25, dense=True):
         self.dense = dense
 
-        self.warmup = int(warmup)
+        # self.warmup = int(warmup)
         self.start = int(start)
         self.finish = int(finish)
         self.window = int(window)
+        self.count = 0
+        self._last_window = 0
 
-        if self.start + self.finish + self.window > self.warmup:
-            logging.warning("not enough warmup samples for requested "
-                            "tuning schedule")
-            start = int(0.15 * self.warmup)
-            finish = int(0.1 * self.warmup)
-            window = int(warmup - start - finish)
+        # if self.start + self.finish + self.window > self.warmup:
+        #     logging.warning("not enough warmup samples for requested "
+        #                     "tuning schedule")
+        #     start = int(0.15 * self.warmup)
+        #     finish = int(0.1 * self.warmup)
+        #     window = int(warmup - start - finish)
 
-        tune = self.warmup - self.start - self.finish
-        if self.window + 2 * self.window >= tune:
-            windows = [tune]
-        else:
-            windows = [self.window]
-        count = sum(windows)
-        while count < tune:
-            next_window = windows[-1] * 2
-            if count + next_window + 2 * next_window >= tune:
-                next_window = tune - count
-            windows.append(next_window)
-            count += next_window
-        self.windows = np.array(windows, dtype=int)
+        # tune = self.warmup - self.start - self.finish
+        # if self.window + 2 * self.window >= tune:
+        #     windows = [tune]
+        # else:
+        #     windows = [self.window]
+        # count = sum(windows)
+        # while count < tune:
+        #     next_window = windows[-1] * 2
+        #     if count + next_window + 2 * next_window >= tune:
+        #         next_window = tune - count
+        #     windows.append(next_window)
+        #     count += next_window
+        # self.windows = np.array(windows, dtype=int)
 
         self._current_step = None
         self._current_trace = None
@@ -84,9 +85,8 @@ class TuningSchedule(object):
 
         return pm.NUTS(potential=potential, **kwargs)
 
-    def tune(self, start=None, step_kwargs=None, **kwargs):
-        if step_kwargs is None:
-            step_kwargs = {}
+    def _extend(self, steps, start=None, step=None,
+                **kwargs):
 
         kwargs["compute_convergence_checks"] = False
         kwargs["discard_tuned_samples"] = False
@@ -97,31 +97,24 @@ class TuningSchedule(object):
         propagate = logger.propagate
         logger.propagate = False
 
-        # Start by running the startup
-        logging.info("running start-up...")
-        step = self.get_step_for_trace(**step_kwargs)
-
-        burnin_trace = pm.sample(
-            start=start, tune=self.start, step=step, **kwargs)
-        start = [t[-1] for t in burnin_trace._straces.values()]
-
-        for i, steps in enumerate(self.windows):
-            logging.info("running adaptation {0} / {1}..."
-                         .format(i+1, len(self.windows)))
-            step = self.get_step_for_trace(burnin_trace, **step_kwargs)
-            burnin_trace = pm.sample(
-                start=start, tune=steps, step=step, **kwargs)
-            start = [t[-1] for t in burnin_trace._straces.values()]
+        self._current_trace = pm.sample(
+            start=start, tune=steps, step=step, **kwargs)
+        start = [t[-1] for t in self._current_trace._straces.values()]
+        self.count += steps
 
         logger.propagate = propagate
 
-        self._current_trace = burnin_trace
-        self._current_step = self.get_step_for_trace(burnin_trace,
+    def warmup(self, start=None, step_kwargs=None, **kwargs):
+        if step_kwargs is None:
+            step_kwargs = {}
+        step = self.get_step_for_trace(**step_kwargs)
+        self._extend(self.start, start=start, step=step, **kwargs)
+        self._current_step = self.get_step_for_trace(self._current_trace,
                                                      **step_kwargs)
         return self._current_trace
 
-    def sample(self, trace=None, step=None, start=None, step_kwargs=None,
-               **kwargs):
+    def _get_start_and_step(self, start=None, step_kwargs=None, trace=None,
+                            step=None):
         if step_kwargs is None:
             step_kwargs = {}
         if trace is not None:
@@ -134,6 +127,33 @@ class TuningSchedule(object):
         if start is None:
             if trace is not None:
                 start = [t[-1] for t in trace._straces.values()]
+        return start, step
 
+    def extend_tune(self, steps, start=None,
+                    step_kwargs=None, trace=None, step=None, **kwargs):
+        if step_kwargs is None:
+            step_kwargs = {}
+        start, step = self._get_start_and_step(
+            start=start, step_kwargs=step_kwargs, trace=trace, step=step)
+        self._extend(steps, start=start, step=step, **kwargs)
+        self._current_step = self.get_step_for_trace(self._current_trace,
+                                                     **step_kwargs)
+        return self._current_trace
+
+    def tune(self, tune=1000, start=None, step_kwargs=None, **kwargs):
+        self.warmup(start=start, step_kwargs=step_kwargs, **kwargs)
+        steps = self.window
+        while self.count < tune:
+            self.extend_tune(start=start, step_kwargs=step_kwargs, steps=steps,
+                             **kwargs)
+            steps *= 2
+            if self.count + steps + steps*2 > tune:
+                steps = tune - self.count
+        return self._current_trace
+
+    def sample(self, trace=None, step=None, start=None, step_kwargs=None,
+               **kwargs):
+        start, step = self._get_start_and_step(
+            start=start, step_kwargs=step_kwargs, trace=trace, step=step)
         kwargs["tune"] = self.finish
         return pm.sample(start=start, step=step, **kwargs)
