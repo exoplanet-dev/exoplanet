959484759c1990d1e7d4a33366eead0a7c79b33d
Dan F-M <foreman.mackey@gmail.com>
2018-11-18 12:04:26 -0500

    refactoring and trying to optimize Kepler solver

--- exoplanet/theano_ops/kepler/solver.py
+++ exoplanet/theano_ops/kepler/solver.py
@@ -26,19 +26,44 @@ class KeplerOp(gof.COp):
     def make_node(self, mean_anom, eccen):
         in_args = [tt.as_tensor_variable(mean_anom),
                    tt.as_tensor_variable(eccen)]
-        return gof.Apply(self, in_args, [in_args[0].type()])
+        return gof.Apply(self, in_args, [in_args[0].type(), in_args[0].type()])
 
     def infer_shape(self, node, shapes):
-        return shapes[0],
+        return shapes[0], shapes[0]
 
     # def c_code_cache_version(self):
     #     return (0, 0, 1)
 
     def grad(self, inputs, gradients):
         M, e = inputs
-        E = self(M, e)
-        bM = gradients[0] / (1.0 - e * tt.cos(E))
-        be = tt.sin(E) * bM
+        E, f = self(M, e)
+
+        bM = tt.zeros_like(M)
+        be = tt.zeros_like(M)
+        ecosE = e * tt.cos(E)
+
+        if not isinstance(gradients[0].type, theano.gradient.DisconnectedType):
+            # Backpropagate E_bar
+            bM = gradients[0] / (1 - ecosE)
+            be = tt.sin(E) * bM
+
+        if not isinstance(gradients[1].type, theano.gradient.DisconnectedType):
+            # Backpropagate f_bar
+            sinf2 = tt.sin(0.5*f)
+            cosf2 = tt.cos(0.5*f)
+            tanf2 = sinf2 / cosf2
+            e2 = e**2
+            ome2 = 1 - e2
+            ome = 1 - e
+            ope = 1 + e
+            cosf22 = cosf2**2
+            twoecosf22 = 2 * e * cosf22
+            factor = tt.sqrt(ope/ome)
+            inner = (twoecosf22+ome) * tt.as_tensor_variable(gradients[1])
+
+            bM += factor*(ome*tanf2**2+ope)*inner*cosf22/(ope*ome2)
+            be += -2*cosf22*tanf2/ome2**2*inner*(ecosE-2+e2)
+
         return [bM, be]
 
     def R_op(self, inputs, eval_points):
