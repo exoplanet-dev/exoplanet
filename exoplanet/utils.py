1aa462b712d3eb986cfbe66b7614482d3d515cad
Dan F-M <foreman.mackey@gmail.com>
2018-11-17 23:00:58 -0500

    refactoring kepler

--- exoplanet/utils.py
+++ exoplanet/utils.py
@@ -9,10 +9,13 @@ import theano
 from pymc3.model import modelcontext
 
 
-def eval_in_model(var, point=None, model=None):
+def eval_in_model(var, point=None, model=None, return_func=False, **kwargs):
     model = modelcontext(model)
     if point is None:
         point = model.test_point
-    func = theano.function(model.vars, var, on_unused_input="ignore")
-    args = (point[k.name] for k in model.vars)
+    kwargs["on_unused_input"] = kwargs.get("on_unused_input", "ignore")
+    func = theano.function(model.vars, var, **kwargs)
+    args = [point[k.name] for k in model.vars]
+    if return_func:
+        return func(*args), func, args
     return func(*args)
