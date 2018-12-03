8e797db91972b086e14ac3ecd0b140d9d5919b88
Dan F-M <foreman.mackey@gmail.com>
2018-12-03 13:37:49 -0500

    adding stellar variability tutorial

--- exoplanet/theano_ops/kepler/include/contact_points.h
+++ exoplanet/theano_ops/kepler/include/contact_points.h
@@ -1,17 +1,18 @@
 #ifndef _CONTACT_POINTS_H_
 #define _CONTACT_POINTS_H_
 
-#include <vector>
 #include <cmath>
 #include <algorithm>
 
 namespace contact_points {
+
 template <typename T>
 class ContactPoint {
   private:
-    T e, sinw, cosw, cosi2, factor, b2_0;
+    T e, sinw, cosw, cosi2, factor, b2_0, f0, Efactor;
 
     inline T b2 (T delta) {
+      n_eval ++;
       T sd = sin(delta);
       T cd = cos(delta);
       T sdmw = cosw * sd - sinw * cd;  // sin(delta - w)
@@ -35,8 +36,9 @@ class ContactPoint {
     }
 
   public:
+    int n_eval;
 
-    ContactPoint (T a, T e, T w, T i) : e(e) {
+    ContactPoint (T a, T e, T w, T i) : e(e), n_eval(0) {
       sinw = sin(w);
       cosw = cos(w);
       cosi2 = pow(cos(i), 2);
@@ -47,41 +49,55 @@ class ContactPoint {
 
       T arg = e * sinw - 1;
       b2_0 = factor * cosi2 / (arg * arg);
+
+      f0 = M_PI + 2 * atan2(cosw, 1 + sinw);
+      Efactor = sqrt((1 - e) / (1 + e));
     }
 
-    int find_contacts (T R, T r, T* f1, T* f2, T* f3, T* f4, int maxiter, T tol) {
+    inline T df_to_M (T df) {
+      T E = 2 * atan(Efactor * tan(0.5*(f0 + df)));
+      return E - e * sin(E);
+    }
+
+    int find_contacts (T R, T r, T* M1, T* M2, T* M3, T* M4, int maxiter, T tol) {
       T minus = -0.5 * M_PI;
       T plus = 0.5 * M_PI;
       T b2_minus = b2(minus);
       T b2_plus = b2(plus);
-      T R2;
+      T R2, f1, f2, f3, f4;
 
       R2 = (R + r) * (R + r);
       if (R2 >= b2_minus || R2 <= b2_0) {
-        *f1 = minus;
+        f1 = minus;
       } else {
-        *f1 = find_root(R2, minus, 0, true, maxiter, tol);
+        f1 = find_root(R2, minus, 0, true, maxiter, tol);
       }
       if (R2 >= b2_plus || R2 <= b2_0) {
-        *f4 = plus;
+        f4 = plus;
       } else {
-        *f4 = find_root(R2, 0, plus, false, maxiter, tol);
+        f4 = find_root(R2, 0, plus, false, maxiter, tol);
       }
 
       R2 = (R - r) * (R - r);
       if (R2 >= b2_minus || R2 <= b2_0) {
-        *f2 = 0;
+        f2 = 0;
       } else {
-        *f2 = find_root(R2, *f1, 0, true, maxiter, tol);
+        f2 = find_root(R2, f1, 0, true, maxiter, tol);
       }
       if (R2 >= b2_plus || R2 <= b2_0) {
-        *f3 = 0;
+        f3 = 0;
       } else {
-        *f3 = find_root(R2, 0, *f4, false, maxiter, tol);
+        f3 = find_root(R2, 0, f4, false, maxiter, tol);
       }
+
+      *M1 = df_to_M(f1);
+      *M2 = df_to_M(f2);
+      *M3 = df_to_M(f3);
+      *M4 = df_to_M(f4);
 
       return 0;
     }
+
 };
 
 }  // namespace contact_points
