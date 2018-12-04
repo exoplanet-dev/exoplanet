32bf13b753e84fa638e4fb672bef257b2c88c774
Dan F-M <foreman.mackey@gmail.com>
2018-12-04 11:02:58 -0500

    fixing all the contact point bugs

--- exoplanet/theano_ops/kepler/__init__.py
+++ exoplanet/theano_ops/kepler/__init__.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 
-__all__ = ["KeplerOp"]
+__all__ = ["KeplerOp", "CircularContactPointsOp", "ContactPointsOp"]
 
 from .solver import KeplerOp
+from .contact_points import CircularContactPointsOp, ContactPointsOp
