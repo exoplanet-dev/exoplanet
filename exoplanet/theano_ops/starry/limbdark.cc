df1bdf4c1a0e87f042feabf7e4dd82a2c2832e1a
Dan F-M <foreman.mackey@gmail.com>
2018-12-05 10:01:31 -0500

    refactor of limbdark

--- exoplanet/theano_ops/starry/limbdark.cc
+++ exoplanet/theano_ops/starry/limbdark.cc
@@ -1,11 +1,15 @@
 #section support_code_apply
 
 int APPLY_SPECIFIC(limbdark)(
-    PyArrayObject* input0,  // Array of "cl"
-    PyArrayObject* input1,  // Array of impact parameters "b"
-    PyArrayObject* input2,  // Array of radius ratios "r"
-    PyArrayObject* input3,  // Array of line-of-sight position "los"
-    PyArrayObject** output0)
+    PyArrayObject* input0,    // Array of "cl"
+    PyArrayObject* input1,    // Array of impact parameters "b"
+    PyArrayObject* input2,    // Array of radius ratios "r"
+    PyArrayObject* input3,    // Array of line-of-sight position "los"
+    PyArrayObject** output0,  // Flux
+    PyArrayObject** output1,  // dfdcl
+    PyArrayObject** output2,  // dfdb
+    PyArrayObject** output3   // dfdr
+  )
 {
   npy_intp Nc, Nb, Nr, Nlos;
   int success = get_size(input0, &Nc);
@@ -18,28 +22,54 @@ int APPLY_SPECIFIC(limbdark)(
     return 1;
   }
 
-  success += allocate_output(PyArray_NDIM(input1), PyArray_DIMS(input1), TYPENUM_OUTPUT_0, output0);
+  npy_intp ndim = PyArray_NDIM(input1);
+  npy_intp* dims = PyArray_DIMS(input1);
+  std::vector<npy_intp> shape(ndim + 1);
+  shape[0] = Nc;
+  for (npy_intp i = 0; i < ndim; ++i) shape[i+1] = dims[i];
+
+  success += allocate_output(ndim, dims, TYPENUM_OUTPUT_0, output0);
+  success += allocate_output(ndim+1, &(shape[0]), TYPENUM_OUTPUT_1, output1);
+  success += allocate_output(ndim, dims, TYPENUM_OUTPUT_2, output2);
+  success += allocate_output(ndim, dims, TYPENUM_OUTPUT_3, output3);
   if (success) {
     return 1;
   }
 
-  DTYPE_INPUT_0*  c  = (DTYPE_INPUT_0*) PyArray_DATA(input0);
-  DTYPE_INPUT_1*  b  = (DTYPE_INPUT_1*) PyArray_DATA(input1);
-  DTYPE_INPUT_2*  r  = (DTYPE_INPUT_2*) PyArray_DATA(input2);
+  DTYPE_INPUT_0* c   = (DTYPE_INPUT_0*) PyArray_DATA(input0);
+  DTYPE_INPUT_1* b   = (DTYPE_INPUT_1*) PyArray_DATA(input1);
+  DTYPE_INPUT_2* r   = (DTYPE_INPUT_2*) PyArray_DATA(input2);
   DTYPE_INPUT_3* los = (DTYPE_INPUT_3*) PyArray_DATA(input3);
-  DTYPE_OUTPUT_0* f  = (DTYPE_OUTPUT_0*)PyArray_DATA(*output0);
+
+  DTYPE_OUTPUT_0* f     = (DTYPE_OUTPUT_0*)PyArray_DATA(*output0);
+  DTYPE_OUTPUT_1* dfdcl = (DTYPE_OUTPUT_1*)PyArray_DATA(*output1);
+  DTYPE_OUTPUT_2* dfdb  = (DTYPE_OUTPUT_2*)PyArray_DATA(*output2);
+  DTYPE_OUTPUT_3* dfdr  = (DTYPE_OUTPUT_3*)PyArray_DATA(*output3);
+
+  Eigen::Map<Eigen::Matrix<DTYPE_OUTPUT_1, Eigen::Dynamic, Eigen::Dynamic, Eigen::RowMajor>> dfdcl_mat(dfdcl, Nc, Nb);
+  dfdcl_mat.setZero();
 
   Eigen::Map<Eigen::Matrix<DTYPE_INPUT_0, Eigen::Dynamic, 1>> cvec(c, Nc);
   starry::limbdark::GreensLimbDark<DTYPE_OUTPUT_0> L(Nc-1);
 
   for (npy_intp i = 0; i < Nb; ++i) {
     f[i] = 0;
+    dfdb[i] = 0;
+    dfdr[i] = 0;
+
     if (los[i] < 0) {
       auto b_ = std::abs(b[i]);
       auto r_ = std::abs(r[i]);
       if (b_ < 1 + r_) {
-        L.compute(b_, r_, false);
+        L.compute(b_, r_, true);
+
+        // The value of the light curve
         f[i] = L.S.dot(cvec) - 1;
+
+        // The gradients
+        dfdcl_mat.col(i) = L.S;
+        dfdb[i] = sgn(b[i]) * L.dSdb.dot(cvec);
+        dfdr[i] = sgn(r[i]) * L.dSdr.dot(cvec);
       }
     }
   }
--- exoplanet/theano_ops/starry/limbdark_test.py
+++ exoplanet/theano_ops/starry/limbdark_test.py
@@ -9,7 +9,6 @@ import theano.tensor as tt
 from theano.tests import unittest_tools as utt
 
 from .limbdark import LimbDarkOp
-from .limbdark_rev import LimbDarkRevOp
 
 
 class TestLimbDark(utt.InferShapeTester):
@@ -24,7 +23,7 @@ class TestLimbDark(utt.InferShapeTester):
         b = tt.vector()
         r = tt.vector()
         los = tt.vector()
-        f = theano.function([c, b, r, los], self.op(c, b, r, los))
+        f = theano.function([c, b, r, los], self.op(c, b, r, los)[0])
 
         c_val = np.array([-0.85, 2.5, -0.425, 0.1])
         b_val = np.linspace(-1.5, 1.5, 100)
@@ -48,45 +47,11 @@ class TestLimbDark(utt.InferShapeTester):
     def test_infer_shape(self):
         f, args, arg_vals = self.get_args()
         self._compile_and_check(args,
-                                [self.op(*args)],
+                                self.op(*args),
                                 arg_vals,
                                 self.op_class)
 
     def test_grad(self):
         _, _, in_args = self.get_args()
-        utt.verify_grad(self.op, in_args)
-
-
-class TestLimbDarkRev(utt.InferShapeTester):
-
-    def setUp(self):
-        super(TestLimbDarkRev, self).setUp()
-        self.op_class = LimbDarkRevOp
-        self.op = LimbDarkRevOp()
-
-    def get_args(self):
-        c = tt.vector()
-        b = tt.vector()
-        r = tt.vector()
-        los = tt.vector()
-        bf = tt.vector()
-        f = theano.function([c, b, r, los, bf], self.op(c, b, r, los, bf))
-
-        c_val = np.array([-0.85, 2.5, -0.425, 0.1])
-        b_val = np.linspace(-1.5, 1.5, 100)
-        r_val = 0.1 + np.zeros_like(b_val)
-        los_val = -np.ones_like(b_val)
-        bf_val = np.ones_like(b_val)
-
-        return f, [c, b, r, los, bf], [c_val, b_val, r_val, los_val, bf_val]
-
-    def test_basic(self):
-        f, _, in_args = self.get_args()
-        f(*in_args)
-
-    def test_infer_shape(self):
-        f, args, arg_vals = self.get_args()
-        self._compile_and_check(args,
-                                self.op(*args),
-                                arg_vals,
-                                self.op_class)
+        func = lambda *args: self.op(*args)[0]  # NOQA
+        utt.verify_grad(func, in_args)
