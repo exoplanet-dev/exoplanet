b340650f576495082a211e4bf1984f7bb9661849
Dan F-M <foreman.mackey@gmail.com>
2018-12-06 15:37:15 -0500

    fixing kepler?

--- exoplanet/theano_ops/kepler/solver.cc
+++ exoplanet/theano_ops/kepler/solver.cc
@@ -32,7 +32,7 @@ int APPLY_SPECIFIC(solver)(
   DTYPE_OUTPUT_0* E_out = (DTYPE_OUTPUT_0*)PyArray_DATA(*output0);
   DTYPE_OUTPUT_1* f_out = (DTYPE_OUTPUT_1*)PyArray_DATA(*output1);
 
-  T M0, M, e, E, delta, sinE, tanE2, denom;
+  T M0, M, e, E, delta, func, esinE, tanE2, denom;
   for (npy_intp n = 0; n < N; ++n) {
     M = M_in[n];
     e = e_in[n];
@@ -56,20 +56,41 @@ int APPLY_SPECIFIC(solver)(
       M -= M0;
 
       E = M + e*sin(M);
-      sinE = sin(E);
+      esinE = e * sin(E);
 
-      for (long n = 0; n < maxiter; ++n) {
-        delta = e * sinE + M;
-        E -= (E - delta) / (fmax(1 - e * cos(E), tol));
-        sinE = sin(E);
-        if (fabs(E - e * sinE - M) <= tol) break;
+      if (e < 0.99) {
+        for (long n = 0; n < maxiter; ++n) {
+          delta = func / (1 - e * cos(E));
+          E -= delta;
+          esinE = e * sin(E);
+          func = E - esinE - M;
+          if (fabs(func) <= tol) break;
+        }
+      } else {
+        // Use a more robust method for large eccentricity
+        for (long n = 0; n < maxiter; ++n) {
+          delta = func / (1 - e * cos(E));
+
+          // If the change is large, that normally means that the gradient is
+          // close to zero. We can jump out of this region with a move of Pi
+          // in the right direction.
+          if (fabs(delta) < M_PI) {
+            E -= delta;
+          } else {
+            E -= M_PI * ((delta >= 0) - (delta < 0));
+          }
+
+          esinE = e * sin(E);
+          func = E - esinE - M;
+          if (fabs(func) <= tol) break;
+        }
       }
 
       // Save the result and compute the true anomaly
       E_out[n] = E + M0;
-      denom = 1 + cos(E);
+      denom = e * (1 + cos(E));
       if (fabs(denom) > tol) {
-        tanE2 = sinE / denom;  // tan(0.5*E)
+        tanE2 = esinE / denom;  // tan(0.5*E)
         f_out[n] = 2 * atan(sqrt((1+e)/(1-e))*tanE2);
       } else {
         f_out[n] = M_PI;
