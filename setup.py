d29fbf75d0b93a4e2f48665cb4b86a39999308bf
Dan F-M <foreman.mackey@gmail.com>
2018-12-06 22:53:42 -0500

    cleaning up

--- setup.py
+++ setup.py
@@ -1,14 +1,12 @@
 #!/usr/bin/env python
 
-import numpy as np
-
 import os
 import sys
 from setuptools import setup
 
 dirname = os.path.dirname(os.path.realpath(__file__))
 sys.path.insert(0, os.path.join(dirname, "exoplanet"))
-from exoplanet_version import __version__
+from exoplanet_version import __version__  # NOQA
 
 
 with open(os.path.join(dirname, "requirements.txt"), "r") as f:
