f0b48762be3108c2f1c9fc95b4ccd87911f78ded
Dan F-M <foreman.mackey@gmail.com>
2018-12-11 10:40:47 -0500

    adding dilution notebook

--- exoplanet/theano_ops/interp/include/theano_helpers.h
+++ exoplanet/theano_ops/interp/include/theano_helpers.h
@@ -1,9 +1,36 @@
 #include <Eigen/Core>
+#include <vector>
 
 #ifndef REGULAR_GRID_NOUT
 #define REGULAR_GRID_NOUT Eigen::Dynamic
-#define REGULAR_GRID_NOUT_ORDER Eigen::ColMajor
+#define REGULAR_GRID_NOUT_ORDER Eigen::RowMajor
 #endif
+#ifndef REGULAR_GRID_NDIM_NOUT
+#define REGULAR_GRID_NDIM_NOUT Eigen::Dynamic
+#define REGULAR_GRID_NDIM_NOUT_ORDER Eigen::RowMajor
+#endif
+
+// adapted from https://academy.realm.io/posts/how-we-beat-cpp-stl-binary-search/
+template <typename T>
+inline npy_intp search_sorted (npy_intp N, const T* const x, const T& value) {
+  npy_intp low = -1;
+  npy_intp high = N;
+  while (high - low > 1) {
+    npy_intp probe = (low + high) / 2;
+    T v = x[probe];
+    if (v > value)
+      high = probe;
+    else
+      low = probe;
+  }
+  return high;
+}
+
+template <typename T1, typename T2>
+inline Eigen::Map<Eigen::Matrix<T2, Eigen::Dynamic, 1>> get_points_grid (PyArrayObject* input) {
+  Eigen::Map<Eigen::Matrix<T1, Eigen::Dynamic, 1>> points((T1*)PyArray_DATA(input), PyArray_SIZE(input));
+  return points.template cast<T2>();
+}
 
 int get_dimensions(PyArrayObject* input, npy_intp* N) {
   if (input == NULL || PyArray_NDIM(input) != 1 || !PyArray_CHKFLAGS(input, NPY_ARRAY_C_CONTIGUOUS)) {
