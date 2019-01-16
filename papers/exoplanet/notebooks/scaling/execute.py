4129f0c9c25e39f0bb5c3bb42dd9cffbc8133b9b
Dan Foreman-Mackey <danfm@nyu.edu>
2019-01-16 16:13:59 -0500

    sacling

--- papers/exoplanet/notebooks/scaling/execute.py
+++ papers/exoplanet/notebooks/scaling/execute.py
@@ -13,6 +13,8 @@ os.environ["OMP_NUM_THREADS"] = "1"
 cpus = os.cpu_count()
 if cpus is None:
     cpus = 1
+else:
+    cpus = cpus // 2
 print(cpus)
 
 if len(sys.argv) > 1:
@@ -26,10 +28,12 @@ exe = [
     "{0}".format(n_planets),
 ]
 procs = []
+files = []
 for version in range(cpus):
+    files.append(open("log_{0}_{1}.log".format(n_planets, version), "a"))
     procs.append(sp.Popen(
         exe + ["{0}".format(version)],
-        stdout=sp.DEVNULL, stderr=sp.PIPE))
+        stdout=files[-1], stderr=files[-1]))
     print(version, "started")
 
 finished = [False for _ in procs]
@@ -45,8 +49,10 @@ try:
                 finished[i] = True
         if all(finished):
             break
-        time.sleep(10)
+        time.sleep(1)
 
 finally:
     for proc in procs:
         proc.kill()
+    for f in files:
+        f.close()
