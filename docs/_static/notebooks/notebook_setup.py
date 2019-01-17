4812e23c6d8c25106f31eb8867a8a9b302cf2a36
Dan Foreman-Mackey <danfm@nyu.edu>
2019-01-17 13:01:16 -0500

    updating notebooks and including workaround for #22

--- docs/_static/notebooks/notebook_setup.py
+++ docs/_static/notebooks/notebook_setup.py
@@ -12,6 +12,12 @@ rcParams["font.family"] = ["sans-serif"]
 rcParams["font.sans-serif"] = ["cmss10"]
 rcParams["axes.unicode_minus"] = False
 
+# Hide deprecation warnings from Theano
 import warnings
 warnings.filterwarnings("ignore", category=DeprecationWarning)
 warnings.filterwarnings("ignore", category=FutureWarning)
+
+# Hide Theano compilelock warnings
+import logging
+logger = logging.getLogger("theano.gof.compilelock")
+logger.setLevel(logging.ERROR)
