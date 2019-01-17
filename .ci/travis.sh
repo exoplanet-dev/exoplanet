a4d16e7db2138704c66cf87a11d606a872267949
Dan F-M <foreman.mackey@gmail.com>
2019-01-17 09:45:42 -0800

    celerite?

--- .ci/travis.sh
+++ .ci/travis.sh
@@ -14,19 +14,13 @@ if ! command -v conda > /dev/null; then
     conda update -q conda
     conda create -q --yes -n test python=$PYTHON_VERSION
     conda activate test
-    conda install -q -c conda-forge pip numpy=$NUMPY_VERSION  scipy astropy setuptools Theano pymc3 pytest starry pybind11
+    conda install -q -c conda-forge pip numpy=$NUMPY_VERSION  scipy astropy setuptools Theano pymc3 pytest starry
     pip install parameterized nose coveralls pytest-cov>=2.6.1
 
     pip uninstall -y batman-package
     git clone https://github.com/lkreidberg/batman.git
     cd batman
     git apply $TRAVIS_BUILD_DIR/.ci/batman-patch.diff
-    python setup.py install
-    cd ..
-
-    pip uninstall -y celerite
-    git clone https://github.com/dfm/celerite.git
-    cd celerite
     python setup.py install
     cd ..
 
