a4da53c09389ca6799779bd59bacad2da106b07a
Dan F-M <foreman.mackey@gmail.com>
2019-01-16 16:30:12 -0800

    starry?

--- .ci/travis.sh
+++ .ci/travis.sh
@@ -14,7 +14,7 @@ if ! command -v conda > /dev/null; then
     conda update -q conda
     conda create -q --yes -n test python=$PYTHON_VERSION
     conda activate test
-    conda install -q -c conda-forge pip numpy=$NUMPY_VERSION  scipy astropy setuptools Theano pymc3 pytest # starry celerite
+    conda install -q -c conda-forge pip numpy=$NUMPY_VERSION  scipy astropy setuptools Theano pymc3 pytest starry # celerite
     pip install parameterized nose coveralls pytest-cov>=2.6.1
     pip uninstall -y batman-package
     git clone https://github.com/lkreidberg/batman.git
