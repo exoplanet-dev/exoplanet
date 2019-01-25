4158fb20dd5d73fd98dc84a1d20caf5ab3c4a9d8
Dan F-M <foreman.mackey@gmail.com>
2019-01-25 09:41:05 -0500

    fixing #22

---
+++ exoplanet/sampling_test.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import division, print_function
+
+import numpy as np
+import pymc3 as pm
+
+from .sampling import PyMC3Sampler
+
+
+def test_tuning():
+    ndim = 5
+    np.random.seed(42)
+
+    sampler = PyMC3Sampler(start=10, window=50, finish=10)
+    with pm.Model() as model:
+        pm.Normal("x", sd=np.exp(np.random.uniform(-10, 0, ndim)), shape=ndim)
+
+        trace = sampler.tune(tune=200)
+
+        start, step = sampler._get_start_and_step(
+            start=None, step=None)
+
+        # Expected step size
+        expected = []
+        for chain in trace._straces.values():
+            expected.append(chain.get_sampler_stats("step_size")[-1])
+        expected = np.mean(expected) * model.ndim**(1./4)
+        assert np.allclose(step.step_size, expected)
+
+        # Make sure that it's not still the default
+        assert not np.allclose(step.step_size, 0.25 / model.ndim ** 0.25)
