7204eef986a661e93a22de57d93ddb08d9948e28
Dan Foreman-Mackey <danfm@nyu.edu>
2019-01-26 11:36:23 -0500

    automating notebook running

--- run_notebooks.sh
+++ run_notebooks.sh
@@ -2,5 +2,15 @@
 
 git checkout master
 git pull origin master
+python setup.py develop
 
 git branch -D auto_notebooks
+git checkout -b auto_notebooks master
+
+cd docs
+conda env export > auto_environment.yml
+
+python run_notebooks.py
+
+git -c user.name='exoplanetbot' -c user.email='exoplanetbot' commit -am "updating notebooks [ci skip]"
+git push -q -f https://dfm:`cat .github_api_key`@github.com/dfm/exoplanet.git auto_notebooks
