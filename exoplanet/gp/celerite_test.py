62097cd6d03d7d7a114df40c499e8aa3f53ff473
Dan F-M <foreman.mackey@gmail.com>
2019-04-14 17:43:10 -0700

    debugging

--- exoplanet/gp/celerite_test.py
+++ exoplanet/gp/celerite_test.py
@@ -69,24 +69,25 @@ def _get_theano_kernel(celerite_kernel):
     raise NotImplementedError()
 
 
-@pytest.mark.parametrize(
-    "celerite_kernel",
-    [
-        "cterms.RealTerm(log_a=0.1, log_c=0.5)",
-        "cterms.RealTerm(log_a=0.1, log_c=0.5) + "
-        "cterms.RealTerm(log_a=-0.1, log_c=0.7)",
-        "cterms.ComplexTerm(log_a=0.1, log_c=0.5, log_d=0.1)",
-        "cterms.ComplexTerm(log_a=0.1, log_b=-0.2, log_c=0.5, log_d=0.1)",
-        "cterms.SHOTerm(log_S0=0.1, log_Q=-1, log_omega0=0.5)",
-        "cterms.SHOTerm(log_S0=0.1, log_Q=1.0, log_omega0=0.5)",
-        "cterms.SHOTerm(log_S0=0.1, log_Q=1.0, log_omega0=0.5) + "
-        "cterms.RealTerm(log_a=0.1, log_c=0.4)",
-        "cterms.SHOTerm(log_S0=0.1, log_Q=1.0, log_omega0=0.5) * "
-        "cterms.RealTerm(log_a=0.1, log_c=0.4)",
-        "cterms.Matern32Term(log_sigma=0.1, log_rho=0.4)",
-    ]
-)
-def test_gp(celerite_kernel, seed=1234):
+# @pytest.mark.parametrize(
+#     "celerite_kernel",
+#     [
+#         "cterms.RealTerm(log_a=0.1, log_c=0.5)",
+#         "cterms.RealTerm(log_a=0.1, log_c=0.5) + "
+#         "cterms.RealTerm(log_a=-0.1, log_c=0.7)",
+#         "cterms.ComplexTerm(log_a=0.1, log_c=0.5, log_d=0.1)",
+#         "cterms.ComplexTerm(log_a=0.1, log_b=-0.2, log_c=0.5, log_d=0.1)",
+#         "cterms.SHOTerm(log_S0=0.1, log_Q=-1, log_omega0=0.5)",
+#         "cterms.SHOTerm(log_S0=0.1, log_Q=1.0, log_omega0=0.5)",
+#         "cterms.SHOTerm(log_S0=0.1, log_Q=1.0, log_omega0=0.5) + "
+#         "cterms.RealTerm(log_a=0.1, log_c=0.4)",
+#         "cterms.SHOTerm(log_S0=0.1, log_Q=1.0, log_omega0=0.5) * "
+#         "cterms.RealTerm(log_a=0.1, log_c=0.4)",
+#         "cterms.Matern32Term(log_sigma=0.1, log_rho=0.4)",
+#     ]
+# )
+def test_gp(celerite_kernel="cterms.RealTerm(log_a=0.1, log_c=0.5)",
+            seed=1234):
     print(1)
     import celerite
     print(2)
