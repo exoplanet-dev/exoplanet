74d1087b8d1731d1cd03e09fd4d62b37a6969a30
Dan F-M <foreman.mackey@gmail.com>
2019-04-19 11:48:56 -0700

    more robust contact point solver?

---
+++ papers/exoplanet/proofs/proof_setup.py
@@ -0,0 +1,11 @@
+get_ipython().magic('matplotlib inline')
+get_ipython().magic('config InlineBackend.figure_format = "retina"')
+
+import matplotlib.pyplot as plt
+
+from matplotlib import rcParams
+rcParams["savefig.dpi"] = 100
+rcParams["figure.dpi"] = 100
+rcParams["font.size"] = 16
+rcParams["text.usetex"] = True
+rcParams["text.latex.preamble"] = r"\usepackage{cmbright}"
