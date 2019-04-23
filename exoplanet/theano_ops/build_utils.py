7551b59529a2f8c2d1bd3c7b9e8c504208fbafb8
Dan F-M <foreman.mackey@gmail.com>
2019-04-23 16:22:49 -0700

    compile args?

--- exoplanet/theano_ops/build_utils.py
+++ exoplanet/theano_ops/build_utils.py
@@ -12,8 +12,7 @@ def get_compile_args(compiler):
     opts = ["-std=c++11", "-O2", "-DNDEBUG"]
     if sys.platform == "darwin":
         opts += ["-stdlib=libc++", "-mmacosx-version-min=10.7"]
-
-    return opts
+    return opts + compiler.compile_args()
 
 
 def get_cache_version():
