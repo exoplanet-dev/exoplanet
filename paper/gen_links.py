44eca3e84fafd43f5f9be7b522b403e7c050fcf8
Dan F-M <foreman.mackey@gmail.com>
2019-04-25 07:52:08 -0700

    hard coded slug

--- paper/gen_links.py
+++ paper/gen_links.py
@@ -1,7 +1,7 @@
 import os
 import subprocess
 hash = subprocess.check_output(['git', 'rev-parse', 'HEAD']).decode("utf-8")[:-1]
-slug = os.getenv("TRAVIS_REPO_SLUG", "dfm/exoplanet")
+slug = "dfm/exoplanet"
 with open("gitlinks.tex", "w") as f:
     print(r"\newcommand{\codelink}[1]{\href{https://github.com/%s/blob/%s/paper/figures/#1.ipynb}{\codeicon}\,\,}" % (slug, hash), file=f)
     print(r"\newcommand{\animlink}[1]{\href{https://github.com/%s/blob/%s/paper/figures/#1.gif}{\animicon}\,\,}" % (slug, hash), file=f)
