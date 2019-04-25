ba934b68a1abf73cd3cbec76c4d0cdc1078a85a2
Dan F-M <foreman.mackey@gmail.com>
2019-04-24 18:39:32 -0700

    simpler travis build

--- .ci/travis.sh
+++ .ci/travis.sh
@@ -2,11 +2,11 @@
 if ! command -v conda > /dev/null; then
 
     if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
-        wget https://repo.continuum.io/miniconda/Miniconda3-latest-MacOSX-x86_64.sh -O miniconda.sh;
+        wget -nv https://repo.continuum.io/miniconda/Miniconda3-latest-MacOSX-x86_64.sh -O miniconda.sh;
     else
         export CXX=g++-4.8
         export CC=gcc-4.8
-        wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O miniconda.sh;
+        wget -nv https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O miniconda.sh;
     fi
     bash miniconda.sh -b -p $HOME/miniconda -u;
     . $HOME/miniconda/etc/profile.d/conda.sh
