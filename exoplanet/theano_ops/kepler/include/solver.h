c568410e222c2c52a139415b3b7201a71d4ef7be
Dan F-M <foreman.mackey@gmail.com>
2019-04-30 07:21:47 -0700

    cleanup

--- exoplanet/theano_ops/kepler/include/solver.h
+++ exoplanet/theano_ops/kepler/include/solver.h
@@ -1,69 +1,19 @@
 #ifndef _EXOPLANET_KEPLER_SOLVER_H_
 #define _EXOPLANET_KEPLER_SOLVER_H_
 
-// A solver for Kepler's equation based on: http://adsabs.harvard.edu/abs/1991CeMDA..51..319N
+// A solver for Kepler's equation based on:
+//
 // Nijenhuis (1991)
+// http://adsabs.harvard.edu/abs/1991CeMDA..51..319N
+//
+// and
+//
+// Markley (1995)
+// http://adsabs.harvard.edu/abs/1995CeMDA..63..101M
 
 #include <cmath>
 
 namespace exoplanet {
-
-  template <typename T>
-  inline T get_starter (T M, T e) {
-
-    const T mikkoleft = 0.45;
-    const T mikkolup = 1 - mikkoleft;
-    const T s3 = -0.16605;
-    const T s5 = 0.00761;
-    const T ds2 = 3 * s3;
-    const T ds4 = 5 * s5;
-
-    if (M <= 0) return T(0);
-
-    T E;
-    T M1 = M + e;
-    T e1 = 1 - e;
-    bool mikkola = false;
-    if (M1 > M_PI - 1) {
-      E = (M + e * M_PI) / (1 + e);
-    } else if (M1 > 1) {
-      if (M > mikkoleft) {
-        E = M1;
-      } else {
-        mikkola = true;
-      }
-    } else if (e < mikkolup) {
-      E = M / e1;
-    } else {
-      mikkola = true;
-    }
-
-    if (mikkola) {
-      T den = 0.5 + 4 * e;
-      T p = e1 / den;
-      T q = 0.5 * M / den;
-      T p2 = p*p;
-      T zsq = exp(log(sqrt(p2*p + q*q) + q) / 1.5);
-      T s = 2 * q / (zsq + p + p2 / zsq);
-      T ssq = s*s;
-      T sqq = ssq * ssq;
-      s -= 0.075 * s * sqq / (e1 + den*ssq + 0.375*sqq);
-      E = M + e * s * (3 - 4 * s*s);
-    } else {
-      bool big = E > M_PI_2;
-      T x = (big) ? M_PI - E : E;
-      T xsq = x*x;
-      T sn = x * (1 + xsq * (s3 + xsq * s5));
-      T dsn = 1 + xsq * (ds2 + xsq * ds4);
-      if (big) dsn = -dsn;
-      T f2 = e * sn;
-      T f0 = E - f2 - M;
-      T f1 = 1 - e * dsn;
-      E -= f0 / (f1 - 0.5 * f0 * f2 / f1);
-    }
-
-    return E;
-  }
 
   // Calculates x - sin(x) and 1 - cos(x) to 20 significant digits for x in [0, pi)
   template <typename T>
@@ -100,75 +50,6 @@ namespace exoplanet {
   }
 
   template <typename T>
-  inline T refine (T E, T M, T e) {
-    T e1 = 1 - e;
-    T snr, csr;
-    sin_cos_reduc(E, &snr, &csr);
-
-    T F[] = {
-      E * e1 + e * snr - M,
-      e1 + e * csr,
-      0.5 * e * (E - snr),
-      e * (1 - csr) / 6, 0};
-    F[4] = -F[2] / 12;
-    T h[4];
-
-    T delta;
-    for (int i = 1; i <= 4; ++i) {
-      delta = F[i];
-      for (int j = 1; j <= i-1; ++j) {
-        delta = delta * h[j-1] + F[i-j];
-      }
-      h[i-1] = -F[0] / delta;
-    }
-    return E + h[4 - 1];
-  }
-
-  //template <typename T>
-  //inline T solve_kepler (T M, T e) {
-  //  const T two_pi = 2 * M_PI;
-
-  //  T M_ref = two_pi * floor(M / two_pi);
-  //  M -= M_ref;
-
-  //  bool high = M > M_PI;
-  //  if (high) {
-  //    M = two_pi - M;
-  //  }
-
-  //  // Initialize
-  //  T E0 = get_starter<T>(M, e);
-
-  //  // Refine the estimate
-  //  T E = refine<T>(E0, M, e);
-
-  //  if (high) {
-  //    E = two_pi - E;
-  //  }
-
-  //  return E + M_ref;
-  //}
-
-  template <typename T>
-  inline T pade_approx (T E, T sinE, T ecc) {
-    if (ecc < 0.5 || E > 1.0) return E - ecc*sinE;
-    T E2 = E*E;
-    T E4 = E2*E2;
-    T E6 = E4*E2;
-    T E8 = E6*E2;
-    T numerator = 1.0
-                  - 1.7454287843856404e-6 * E6
-                  + 4.1584640418181644e-4 * E4
-                  - 3.0956446448551138e-2 * E2;
-    T denominator = 6.0
-                    + 1.7804367119519884e-8 * E8
-                    + 5.9727613731070647e-6 * E6
-                    + 1.0652873476684142e-3 * E4
-                    + 1.1426132130869317e-1 * E2;
-    return (1.0 - ecc) * E + ecc * E2*E * numerator / denominator;
-  }
-
-  template <typename T>
   inline T solve_kepler (T M, T ecc) {
     const T two_pi = 2 * M_PI;
 
@@ -194,15 +75,10 @@ namespace exoplanet {
     T E = (2*r*w/(w*w + w*q + q2) + M) / d;
 
     // Approximate Mstar = E - e*sin(E) with numerically stability
-    //T sinE = sin(E);
-    //T Mstar = pade_approx(E, sinE, ecc);
     T sE, cE;
     sin_cos_reduc (E, &sE, &cE);
 
     // Refine the starter
-    //T f_0 = Mstar - M;
-    //T f_1 = 1 - ecc*cos(E);
-    // T f_2 = ecc * sinE;
     T f_0 = ecc * sE + E * ome - M;
     T f_1 = ecc * cE + ome;
     T f_2 = ecc * (E - sE);
