945d6062e94c9efc63f21f4dd47865f8b82402b1
Dan F-M <foreman.mackey@gmail.com>
2019-07-11 08:55:20 -0400

    css and fixing figures

--- docs/conf.py
+++ docs/conf.py
@@ -13,7 +13,7 @@ sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 
 def setup(app):
-    app.add_stylesheet("css/exoplanet.css?v=6")
+    app.add_stylesheet("css/exoplanet.css?v=3")
 
 
 extensions = [
