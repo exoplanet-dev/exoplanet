f6bfac22885cf7f1e7ba42b9508416b76a1c0c8e
exoplanetbot <exoplanetbot>
2019-07-12 21:00:12 -0400

    updating notebooks [ci skip]

--- docs/_static/notebooks/notebook_setup.py
+++ docs/_static/notebooks/notebook_setup.py
@@ -1,17 +1,6 @@
 get_ipython().magic('matplotlib inline')
 get_ipython().magic('config InlineBackend.figure_format = "retina"')
 
-import matplotlib.pyplot as plt
-
-from matplotlib import rcParams
-rcParams["savefig.dpi"] = 100
-rcParams["figure.dpi"] = 100
-rcParams["font.size"] = 16
-rcParams["text.usetex"] = False
-rcParams["font.family"] = ["sans-serif"]
-rcParams["font.sans-serif"] = ["cmss10"]
-rcParams["axes.unicode_minus"] = False
-
 # Hide deprecation warnings from Theano
 import warnings
 warnings.filterwarnings("ignore", category=DeprecationWarning)
@@ -30,3 +19,12 @@ print("pymc3 version: {0}".format(pymc3.__version__))
 
 import exoplanet
 print("exoplanet version: {0}".format(exoplanet.__version__))
+
+import matplotlib.pyplot as plt
+plt.style.use("default")
+plt.rcParams["savefig.dpi"] = 100
+plt.rcParams["figure.dpi"] = 100
+plt.rcParams["font.size"] = 16
+plt.rcParams["font.family"] = "sans-serif"
+plt.rcParams["font.sans-serif"] = ["Liberation Sans"]
+plt.rcParams["mathtext.fontset"] = "custom"
