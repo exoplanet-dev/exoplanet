da226899e161e1b2d723c6f49ff8b2871fb6fd48
Dan F-M <foreman.mackey@gmail.com>
2019-08-28 11:22:11 -0400

    adding rebound op

---
+++ exoplanet/theano_ops/rebound/__init__.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+
+__all__ = ["ReboundOp"]
+
+from .rebound import ReboundOp
--- exoplanet/theano_ops/starry/limbdark.py
+++ exoplanet/theano_ops/starry/limbdark.py
@@ -1,7 +1,5 @@
 # -*- coding: utf-8 -*-
 
-from __future__ import division, print_function
-
 __all__ = ["LimbDarkOp"]
 
 import theano
