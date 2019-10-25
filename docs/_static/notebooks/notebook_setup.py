28992f1fd666a4a0d2bbcb1f311c757f44ba3c71
exoplanetbot <exoplanetbot>
2019-10-25 10:39:26 -0400

    updating notebooks [ci skip]

--- docs/_static/notebooks/notebook_setup.py
+++ docs/_static/notebooks/notebook_setup.py
@@ -21,4 +21,5 @@ plt.rcParams["figure.dpi"] = 100
 plt.rcParams["font.size"] = 16
 plt.rcParams["font.family"] = "sans-serif"
 plt.rcParams["font.sans-serif"] = ["Liberation Sans"]
+plt.rcParams["font.cursive"] = ["Liberation Sans"]
 plt.rcParams["mathtext.fontset"] = "custom"
