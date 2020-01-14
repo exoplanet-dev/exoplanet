6c38298192a8ebe6228c1d6070826d2204975deb
Dan F-M <foreman.mackey@gmail.com>
2020-01-14 16:07:22 -0500

    try to fix BLAS warnings

--- .ci/azure/run_tutorials.sh
+++ .ci/azure/run_tutorials.sh
@@ -5,4 +5,10 @@ set -e
 . $CONDA/etc/profile.d/conda.sh
 conda activate ./env
 
+# Attempt to fix BLAS warnings
+sudo apt-get update
+sudo apt-get install libblas-dev liblapack-dev libatlas-base-dev gfortran
+export THEANO_FLAGS=blas.ldflags="-L/usr/lib/ -lblas"
+
+# Execute the tutorials
 env/bin/python .ci/azure/run_tutorials.py
