0da6436f24c6e4f665d6423a849071779e9db0ef
Dan F-M <foreman.mackey@gmail.com>
2020-01-16 12:53:11 -0500

    correct flags

--- .ci/azure/run_tutorials.sh
+++ .ci/azure/run_tutorials.sh
@@ -8,11 +8,12 @@ conda activate ./env
 # Attempt to fix BLAS warnings
 sudo apt-get update
 sudo apt-get install libblas-dev liblapack-dev libatlas-base-dev gfortran
-export THEANO_FLAGS=blas.ldflags="-L/usr/lib/ -lblas"
+export THEANO_FLAGS=blas.ldflags="-L/usr/lib/ -lblas",$THEANO_FLAGS
+echo $THEANO_FLAGS
 
 # Execute the tutorials
 mkdir -p docs/_static/notebooks
 cp docs/notebooks/notebook_setup.py docs/_static/notebooks
 env/bin/python .ci/azure/run_tutorials.py
 
-ls theano
+ls
