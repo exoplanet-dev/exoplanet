865924e4cabb1f311639a7c3a91bb8a34f2c54bd
Dan F-M <foreman.mackey@gmail.com>
2020-04-02 18:39:36 -0400

    configuration on windows

--- src/exoplanet/conftest.py
+++ src/exoplanet/conftest.py
@@ -1,8 +1,6 @@
 # -*- coding: utf-8 -*-
 # ref: https://github.com/pymc-devs/pymc3/blob/master/pymc3/tests/conftest.py
 
-import platform
-
 import pytest
 import theano
 
@@ -10,9 +8,10 @@ import theano
 @pytest.fixture(scope="package", autouse=True)
 def theano_config():
     flags = dict(compute_test_value="off")
-    if platform.system() == "Windows":
-        print("windows")
-        flags["mode"] = "FAST_COMPILE"
+    # import platform
+    # if platform.system() == "Windows":
+    #     print("windows")
+    #     flags["mode"] = "FAST_COMPILE"
     config = theano.configparser.change_flags(**flags)
     with config:
         yield
