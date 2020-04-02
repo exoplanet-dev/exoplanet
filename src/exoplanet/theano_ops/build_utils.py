5ab22db967eccde7d2d3107d2ae7b3231459405c
Dan F-M <foreman.mackey@gmail.com>
2020-04-02 12:25:33 -0400

    windows things?

--- src/exoplanet/theano_ops/build_utils.py
+++ src/exoplanet/theano_ops/build_utils.py
@@ -14,7 +14,7 @@ def get_compile_args(compiler):
     if sys.platform == "darwin":
         opts += ["-stdlib=libc++", "-mmacosx-version-min=10.7"]
     if sys.platform.startswith("win"):
-        opts += ["-D_USE_MATH_DEFINES", "-D_hypot=hypot" "-O0"]
+        opts += ["-D_USE_MATH_DEFINES", "-D_hypot=hypot", "-O0"]
     else:
         opts += ["-O2"]
     return opts
