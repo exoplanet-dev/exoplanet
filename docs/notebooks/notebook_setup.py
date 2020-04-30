0cd3d069f83433b71d9d72ef4addf076899cdc3f
Dan F-M <foreman.mackey@gmail.com>
2020-04-30 10:59:30 -0400

    fixing env variable for docs

--- docs/notebooks/notebook_setup.py
+++ docs/notebooks/notebook_setup.py
@@ -9,7 +9,7 @@ import warnings
 import matplotlib.pyplot as plt
 
 # Don't use the schmantzy progress bar
-os.environ["EXOPLANET_NO_AUTO_PBAR"] = True
+os.environ["EXOPLANET_NO_AUTO_PBAR"] = "true"
 
 # Remove when Theano is updated
 warnings.filterwarnings("ignore", category=DeprecationWarning)
