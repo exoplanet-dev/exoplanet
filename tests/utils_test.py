7db0619d4a643eb5da26d5f4effe40a2947436c4
Dan F-M <foreman.mackey@gmail.com>
2020-05-15 08:24:48 -0400

    adding test for optimize exceptions

--- tests/utils_test.py
+++ tests/utils_test.py
@@ -2,6 +2,8 @@
 
 import numpy as np
 import pymc3 as pm
+import pytest
+import theano.tensor as tt
 
 from exoplanet.utils import eval_in_model, optimize
 
@@ -27,3 +29,15 @@ def test_optimize(seed=1234):
     assert np.allclose(soln1["x"], 0.0)
     assert np.allclose(soln2["x"], 0.0)
     assert info.success
+
+
+def test_optimize_exception(capsys):
+    with pm.Model():
+        cov = pm.Normal("cov", mu=np.eye(5), shape=(5, 5))
+        chol = tt.slinalg.Cholesky(on_error="raise")(cov)
+        pm.MvNormal("x", mu=np.zeros(5), chol=chol, shape=5)
+        with pytest.raises(np.linalg.LinAlgError):
+            optimize({"cov": np.zeros((5, 5))}, verbose=False)
+        captured = capsys.readouterr()
+        assert "array:" in captured.out
+        assert "point:" in captured.out
